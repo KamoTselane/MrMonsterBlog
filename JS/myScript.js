a1de47ae4d65c728ebc5d694e04e8eed8bd0a85b
Alutas Theory <53045448+KamoTselane@users.noreply.github.com>
2021-04-04 21:15:15 +0200

    Add files via upload

---
+++ JS/myScript.js
@@ -0,0 +1,23 @@
+function deleteMan1() {
+  document.getElementById("man1").style.display="none";
+}
+
+function deleteMan2() {
+  document.getElementById("man2").style.display="none";
+}
+
+function deleteMan3() {
+  document.getElementById("man3").style.display="none";
+}
+
+function deleteMan4() {
+  document.getElementById("man4").style.display="none";
+}
+
+function deleteMan5() {
+  document.getElementById("man5").style.display="none";
+}
+
+function deleteMan6() {
+  document.getElementById("man6").style.display="none";
+}
